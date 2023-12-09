24e9f8e3f179c11e46f53a3d36545f632f615120
dmitry-rogolev <work.drogolev@internet.ru>
2023-12-09 08:17:10 +0700

    v0.0.1

--- tests/Services/Service.php
+++ tests/Services/Service.php
@@ -2,15 +2,15 @@
 
 namespace dmitryrogolev\Service\Tests\Services;
 
+use dmitryrogolev\Contracts\Resourcable as ResourcableContract;
 use dmitryrogolev\Service\Tests\Database\Seeders\UserSeeder;
 use dmitryrogolev\Service\Tests\Models\User;
-use dmitryrogolev\Contracts\Resourcable as ResourcableContract;
 use dmitryrogolev\Traits\Resourcable;
 
-class Service extends \dmitryrogolev\Services\Service implements ResourcableContract 
+class Service extends \dmitryrogolev\Services\Service implements ResourcableContract
 {
     use Resourcable;
-    
+
     public function __construct()
     {
         parent::__construct(User::class, UserSeeder::class);
